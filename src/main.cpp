5efaeaf87ec82682bade0407fb74c707fcae4ad6
Marc Brunet <marbrunet92@gmail.com>
2025-07-30 11:17:47 +0200

    Refactor GPIO control logic into a separate start function and update executable definition in CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,28 @@
 #include "Phare.hpp"
 #include <vector>
 
+void start() {
+    sleep_ms(2000);
+
+    for (int pin = 0; pin <= 21; ++pin) {
+        gpio_put(pin, 1);  // Met la broche à l'état haut (3.3V)
+    }
+
+    sleep_ms(1000);
+
+    for (int pin = 0; pin <= 21; ++pin) {
+        gpio_put(pin, 0);  // Met la broche à l'état bas (0V)
+    }
+
+    sleep_ms(1000);
+
+    for (int pin = 0; pin <= 21; ++pin) {
+        gpio_put(pin, 1);  // Met la broche à l'état haut (3.3V)
+        sleep_ms(500);
+        gpio_put(pin, 0);  // Met la broche à l'état bas (0V)
+    }
+}
+
 int main() {
     stdio_init_all();
 
@@ -10,8 +32,8 @@ int main() {
         Phare(0, make_group_flashes(3, 15000000)), // La Caravelle
         Phare(1, make_group_flashes(1, 5000000)),  // Pointe Venus
         Phare(2, make_group_flashes(3, 15000000)), // Bel Air
-        Phare(3, make_group_flashes(1, 5000000)),  // Plateau du Four
-        Phare(4, make_group_flashes(4, 15000000)), // Grands Cardinaux
+        Phare(20, make_group_flashes(1, 5000000)),  // Plateau du Four
+        Phare(21, make_group_flashes(4, 15000000)), // Grands Cardinaux
         Phare(5, make_group_flashes(1, 5000000)),  // Eckmühl
         Phare(6, make_iso(4000000)),               // La Vieille
         Phare(7, make_group_flashes(4, 25000000)), // Île de Sein
@@ -25,27 +47,9 @@ int main() {
         Phare(16, make_group_flashes(2, 10000000)),// Cap Fréhel
         Phare(17, make_group_flashes(2, 10000000)) // Grands Jardins
     };
+
+    start();
     
-    sleep_ms(2000);
-
-    for (int pin = 0; pin <= 20; ++pin) {
-        gpio_put(pin, 1);  // Met la broche à l'état haut (3.3V)
-    }
-
-    sleep_ms(2000);
-
-    for (int pin = 0; pin <= 20; ++pin) {
-        gpio_put(pin, 0);  // Met la broche à l'état bas (0V)
-    }
-
-    sleep_ms(2000);
-
-    for (int pin = 0; pin <= 20; ++pin) {
-        gpio_put(pin, 1);  // Met la broche à l'état haut (3.3V)
-        sleep_ms(2000);
-        gpio_put(pin, 0);  // Met la broche à l'état bas (0V)
-    }
-
     while (true) {
         uint64_t now = time_us_64();
         for (auto& phare : phares) {
